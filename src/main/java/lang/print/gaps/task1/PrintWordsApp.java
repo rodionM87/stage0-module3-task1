c491eb3256149c4934c49bcd22ab06432f3a1687
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-02-26 22:47:19 +0600

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        //write your code here
+        Syetem.out.println("java" + "\n is" + "\n +smth.");
     }
 }
